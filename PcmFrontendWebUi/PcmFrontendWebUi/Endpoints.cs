c6423a286051e250c37464bcb7407f82d11eb5e4
dannyNeup <dannynp03@gmail.com>
2022-11-24 14:24:39 +0100

    update Endpoints

--- PcmFrontendWebUi/PcmFrontendWebUi/Endpoints.cs
+++ PcmFrontendWebUi/PcmFrontendWebUi/Endpoints.cs
@@ -4,4 +4,8 @@ public class Endpoints
 {
     public const string BaseUrl = "http://localhost:3001";
     public const string Person = "/person";
+    public const string Article = "/article";
+    public const string Order = "/order";
+    public const string ArticleType = "/articletype";
+    public const string Apprenticeship = "/apprenticeship";
 }
