bfbf9e91f189131f8baca79e73235f477408d003
dannyNeup <dannynp03@gmail.com>
2022-11-24 14:21:19 +0100

    add components ArticleList.razor, PersonList.cs, NewPerson.razor

--- PcmFrontendWebUi/PcmFrontendWebUi/Components/PersonList.cs
+++ PcmFrontendWebUi/PcmFrontendWebUi/Components/PersonList.cs
@@ -1,17 +1,34 @@
 using Microsoft.AspNetCore.Components;
+using PcmFrontendWebUi.Models;
+using PcmFrontendWebUi.Repositories;
 
 namespace PcmFrontendWebUi.Components;
 
 public partial class PersonList : ComponentBase
 {
-    [Inject]
-    public IHttpController HttpController { get; set; }
+    private IEnumerable<Person> _persons = new List<Person>();
 
-    [Parameter] public RenderFragment test { get; set; }
-    private List<Person> _persons = new();
+    private string _searchString = "";
+
+    [Inject] public IRepository<Person, int> PersonRepository { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        _persons = await HttpController.GetAll();
+        _persons = await PersonRepository.GetAll();
+    }
+
+    private bool PersonFilter(Person person)
+    {
+        if (string.IsNullOrWhiteSpace(_searchString))
+            return true;
+        var properties = new List<string>();
+        properties.Add(person.Id.ToString());
+        properties.Add(person.FirstName);
+        properties.Add(person.LastName);
+        properties.Add(person.Apprenticeship.Name);
+        properties.Add(person.EmailAddress);
+        properties.RemoveAll(x => x == null);
+        var matchedProperty = properties.FirstOrDefault(x => x.ToLower().Contains(_searchString.ToLower()));
+        return matchedProperty != null;
     }
 }
